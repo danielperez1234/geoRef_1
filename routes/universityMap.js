170a4edd4abccf469cc873c15f1c53376b30f305
Daniel Perez Salgado <dps72725@udelasalle.edu.mx>
2023-02-16 20:39:06 -0600

    Actualizar universityinfo.txt, gmap.js y 6 más archivos...

--- routes/universityMap.js
+++ routes/universityMap.js
@@ -1,7 +1,26 @@
 var express = require('express');
-var router =express.Router();
+var router = express.Router();
+var fs = require("fs");
 
-router.get('/',function(req,res,next){
-  res.send('for this ')
+var app =express();
+var path = require("path");
+
+router.get('/', function (req, res, next) {
+    res.send('For this section we eill work on displaying a custom map');
+});
+
+router.get('/show', function (req, res, next) {
+    var dataFile;
+
+    fs.readFile(path.join(__dirname).replace('routes', '') + 'public/datafiles/universityinfo.txt', 
+    function (error, data) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            dataFile = data.toString();
+            res.render('map',{data:dataFile})
+        }
+    });
 });
 module.exports = router;
